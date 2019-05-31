998e7314da8b603654b31c0cd3948315059c600f
林星河 <gagagaseiga@gmail.com>
2019-05-31 13:02:54 +0900

    [feat]モック作成

--- app/api/articles.rb
+++ app/api/articles.rb
@@ -9,7 +9,12 @@ class Articles < Grape::API
       requires :content, type: String, documentation: {param_type: 'body'}, desc: '新規記事本文', default: '新規記事本文例'
     end
     post do
-
+      response = {
+        "id": 1,
+        "title": "記事タイトル例",
+        "content": "記事内容例",
+      }
+      present response, with: Entities::ArticleEntity
     end
   end
 end
